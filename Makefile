e1ed853033ec0dd53c1d32c5707c41881f30d77f
Tyler Stapler <tstapler@iastate.edu>
2015-09-20 21:01:53 -0500

    Updated makefile for seeding the database.
    Deleted some tswift :'( from the seed data.
    
    Updated dependencies
    
    Insert websocket listener to runtime

--- Makefile
+++ Makefile
@@ -8,3 +8,4 @@ reset:
 	python manage.py delete_db
 	python manage.py create_db
 	python manage.py seed_db --seedfile data/db_items.json
+	python manage.py seed_db --seedfile data/db_items2.json
--- runserver.py
+++ runserver.py
@@ -1,10 +1,14 @@
 import os
-from angular_flask import app
-
+from geventwebsocket.handler import WebSocketHandler
+from gevent.pywsgi import WSGIServer
+from flask import Flask, request, render_template
+from angular_flask import app,my_app
 
 def runserver():
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
+    http_server = WSGIServer(('',5000), my_app, handler_class=WebSocketHandler)
+    http_server.serve_forever()
     runserver()
