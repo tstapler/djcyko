6b29b6d3ee988daedb8c7e0d36c2887afd0aa374
Tyler Stapler <tstapler@iastate.edu>
2015-12-24 01:37:42 -0600

    Changed config to use environ variables

--- angular_flask/settings.py
+++ angular_flask/settings.py
@@ -1,10 +1,13 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/angular_flask.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',  default='sqlite:////tmp/angular_flask_testing.db')
     SECRET_KEY = 'temporary_secret_key'  # make sure to change this
 
 class ProductionConfig(Config):
+    SECRET_KEY = os.getenv('SECRET_KEY', default='This isnt secret') # make sure to change this
     pass
 
 class DevelopmentConfig(Config):
