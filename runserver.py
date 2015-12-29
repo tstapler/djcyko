c0a5d3201cec92e8eb96fbd21969645ac7f7029b
Tyler Stapler <tstapler@iastate.edu>
2015-12-28 19:18:39 -0600

    App again runs with debugger when launched in Dev mode

--- runserver.py
+++ runserver.py
@@ -1,13 +1,25 @@
 from socketio.server import SocketIOServer
-from angular_flask import app, socketio
+from werkzeug.debug import DebuggedApplication
+import werkzeug.serving
 from gevent import monkey
 import click
+
+monkey.patch_all()
+from angular_flask import app, socketio
+
+
+def run_server(use_app=app, port=5000):
+    SocketIOServer(('',port),use_app,resource="socket.io").serve_forever()
+
+@werkzeug.serving.run_with_reloader
+def run_debug_server(use_app=app, port=5000):
+    app = DebuggedApplication(use_app, evalex=True)
+    SocketIOServer(('',port),app,resource="socket.io").serve_forever()
 
 @click.command()
 @click.option('--config',default="p", type=click.Choice(['p','d','t']), help='Type of Configuration')
 @click.option('--port',default=5000, help='The Port the webserver should run on')
-def run_server(config, port):
-    print(port)
+def configure(config, port):
     if config == "p":
         app.config.from_object('angular_flask.settings.ProductionConfig')
     elif config == "d":
@@ -16,9 +28,12 @@ def run_server(config, port):
         app.config.from_object('angular_flask.settings.TestingConfig')
     elif config == "n":
         pass
+    if config == "d":
+        run_debug_server(app, port)
+    else:
+        run_server(app, port)
 
-    monkey.patch_all()
-    SocketIOServer(('',port),app,resource="socket.io").serve_forever()
+
 
 if __name__ == '__main__':
-    run_server()
+    configure()
